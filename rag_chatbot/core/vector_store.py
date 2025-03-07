5286e51ab8d316001731d387cef0b8d2a0d0a7df
Marco Martelli <marcomartelli912@gmail.com>
2025-03-07 11:35:39 +0100

    Refactor VectorStoreManager to implement singleton pattern and ensure proper initialization

--- rag_chatbot/core/vector_store.py
+++ rag_chatbot/core/vector_store.py
@@ -12,13 +12,25 @@ from pathlib import Path
 from config import Config as cfg
 
 class VectorStoreManager:
+    '''
+    Singleton class that manages the vector store.'''
+    _instance = None # instance of the class
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(VectorStoreManager, cls).__new__(cls)
+            cls._instance._initialized = False  # Flag to check if the class has been initialized
+        return cls._instance
+
     def __init__(self, documents_path=cfg.DOCUMENTS_PATH, vector_store_path=cfg.VECTOR_STORE_PATH):
-        self.documents_path = documents_path
-        self.vector_store_path = vector_store_path
-        self.docs = []
-        self.chunks = []
-        self.vector_store = None
-        self.embeddings = MistralAIEmbeddings(model=cfg.EMBEDDING_MODEL)
+        if not self._initialized:
+            self.documents_path = documents_path
+            self.vector_store_path = vector_store_path
+            self.docs = []
+            self.chunks = []
+            self.vector_store = None
+            self.embeddings = MistralAIEmbeddings(model=cfg.EMBEDDING_MODEL)
+            self._initialized = True  # Set the flag to True
 
     def _load_documents(self):
         pdf_files = list(Path(self.documents_path).glob('*.pdf'))
@@ -30,19 +42,15 @@ class VectorStoreManager:
                 print(f"Error loading {pdf_file}: {e}")
         print(f"Successfully loaded {len(self.docs)} documents.")
 
-
     def _split_documents(self):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=cfg.CHUNK_SIZE, chunk_overlap=cfg.CHUNK_OVERLAP)
         self.chunks = text_splitter.split_documents(self.docs)
         print(f'Before split: {len(self.docs)} pages, after split: {len(self.chunks)} chunks.')
-        # Aggiungi questo per controllare i chunk
-        for i, chunk in enumerate(self.chunks[:5]):  # Controlla solo i primi 5 per esempio
-            print(f"Chunk {i}: {chunk.page_content[:300]}")
 
     def load_or_generate_vector_store(self):
         if Path(self.vector_store_path).exists():
             print(f'Loading existing vector store from {self.vector_store_path}')
-            self.vector_store = FAISS.load_local(self.vector_store_path, self.embeddings,allow_dangerous_deserialization=True)
+            self.vector_store = FAISS.load_local(self.vector_store_path, self.embeddings, allow_dangerous_deserialization=True)
         else:
             print('Vector store not found. Generating new one...')
             self._load_documents()
